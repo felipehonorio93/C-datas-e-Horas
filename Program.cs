1e2c1f356b2ffb0071061700462f08f35d60c319
Felipe Honorio <32677477+felipehonorio93@users.noreply.github.com>
2019-08-09 15:48:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2019, 6, 29);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
